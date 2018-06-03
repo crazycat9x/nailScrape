a334c8ee4580e94906a7e40a20d913567cc9d4a4
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-06-03 19:05:06 +1200

    implement callback after last async func

--- index.js
+++ index.js
@@ -6,40 +6,50 @@ const url = "https://vnznailandbeautysupplies.co.nz";
 var json = {};
 
 function retrieveProduct(data) {
-  return new Promise(async resolve => {
-    let $ = cheerio.load(data);
-    let nextPage = $("ul.pagination > li:last-child > a").attr("href");
-    let arr = [];
-    $(".product-name").each((index, element) => {
-      arr.push(
-        $(element)
-          .text()
-          .trim()
-      );
-    });
-    if (nextPage != undefined) {
-      let res = await axios.get(url + nextPage);
-      let nextPageProduct = await retrieveProduct(res.data)
-      let result = await arr.concat(nextPageProduct);
-      resolve(result);
-    } else {
-      resolve(arr);
-    }
-  });
+	return new Promise(async resolve => {
+		let $ = cheerio.load(data);
+		let nextPage = $("ul.pagination > li:last-child > a").attr("href");
+		let arr = [];
+		$(".product-name").each((index, element) => {
+			arr.push(
+				$(element)
+					.text()
+					.trim()
+			);
+		});
+		if (nextPage != undefined) {
+			let res = await axios.get(url + nextPage);
+			let nextPageProduct = await retrieveProduct(res.data);
+			let result = await arr.concat(nextPageProduct);
+			resolve(result);
+		} else {
+			resolve(arr);
+		}
+	});
 }
 
 async function main() {
-  let res = await axios.get(url);
-  let data = res.data;
-  let $ = cheerio.load(data);
-  let menu = $(".sub-mainmenu > li > a");
-  menu.each(async function(index, element) {
-    let res = await axios.get(url + $(element).attr("href"));
-    let product = await retrieveProduct(res.data);
-    console.log(product)
-  });
+	let res = await axios.get(url);
+	let data = res.data;
+	let $ = cheerio.load(data);
+    let menu = $(".sub-mainmenu > li > a");
+    let menuLength = menu.length
+    let count = 0
+	menu.each(async function(index, element) {
+		let title = $(element)
+			.find("span")
+			.text();
+        let res = await axios.get(url + $(element).attr("href"));
+        let product = await retrieveProduct(res.data);
+        json[title] = product
+        count += 1
+        count == menuLength && console.log(json)
+    });
 }
 
 main();
+
+
+// console.log(json);
 
 // fs.writeFile("myjsonfile.json", json, "utf8");
