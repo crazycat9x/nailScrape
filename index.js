43ca9230785a60749124f67c590bd3c39df8adce
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-06-03 19:22:35 +1200

    fix async bug + output to JSON

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const url = "https://vnznailandbeautysupplies.co.nz";
 
-var json = {};
-
 function retrieveProduct(data) {
 	return new Promise(async resolve => {
 		let $ = cheerio.load(data);
@@ -28,27 +26,38 @@ function retrieveProduct(data) {
 	});
 }
 
+function outputToJSON(json, path) {
+	let data = JSON.stringify(json);
+	fs.writeFile(path, data, "utf8", err => {
+		if (err) {
+			return console.log(err);
+		}
+		console.log("The file was saved!");
+	});
+}
+
 async function main() {
+	let json = {};
 	let res = await axios.get(url);
 	let data = res.data;
 	let $ = cheerio.load(data);
-    let menu = $(".sub-mainmenu > li > a");
-    let menuLength = menu.length
-    let count = 0
+	let menu = $(".sub-mainmenu > li > a");
+	let menuLength = menu.length;
+	let count = 0;
 	menu.each(async function(index, element) {
 		let title = $(element)
 			.find("span")
 			.text();
-        let res = await axios.get(url + $(element).attr("href"));
-        let product = await retrieveProduct(res.data);
-        json[title] = product
-        count += 1
-        count == menuLength && console.log(json)
-    });
+		let res = await axios.get(url + $(element).attr("href"));
+		let product = await retrieveProduct(res.data);
+		json[title] = product;
+		count += 1;
+		console.log(count);
+		count == menuLength && outputToJSON(json, "nailData.json");
+	});
 }
 
 main();
-
 
 // console.log(json);
 
